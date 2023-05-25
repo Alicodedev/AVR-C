2910ee1fcbd38798ee332c715bb59fbec7c82d97
Alicodedev <107947617+Alicodedev@users.noreply.github.com>
2023-05-25 15:36:18 -0400

    Pointer_swap function setup

--- Bit-masking_BLINK/Pointer_PIN_SWAP.ino
+++ Bit-masking_BLINK/Pointer_PIN_SWAP.ino
@@ -2,6 +2,8 @@
 int LED1 = 5;
 int LED2 = 6;
 
+void swap(int* a, int* b); // function prototype
+
 void setup() {
   
   pinMode(LED1,OUTPUT);
@@ -18,8 +20,17 @@ void loop() {
   if (!digitalRead(buttonpin) == HIGH) {// if button closed detected *negates high voltage from PULLUP register 
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, LOW);
-    // call swap function
+    
+    swap(&LED1,&LED2);// call swap function
     Serial.println("Button pressed!");
     
   }
+}
+
+void swap(int* a, int* b)// function declaration 
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+    
 }
